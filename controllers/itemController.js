8d7775413e70ffb78f8a82ce5755f64e9d798491
buchananc <candacebuchanan7@gmail.com>
2018-08-10 18:24:30 -0400

    I believe item routes are working

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,37 +3,32 @@
 // controller -> apiRoute -> utils(frontend) -> components(jsx)
 // when a callback happens (goes backward)
 
-const express = require("express");
-const mongoose = require("mongoose");
 const db = require("../models");
-const router = express.Router();
-// const Item = require("../models/Item");
-
-// router.get("/items", function(req,res))
 
 module.exports = {
 
     // queries all items
-    // .get(controllers.itemController.findAll)
+    // .get(itemController.findAll)
     findAll: (req, res) => {
         db.Item
-            .find({})
+            .find(req.query)  // may have to leave brackets empty to show all items
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
 
 
     // queries one item by it's upc
-    // .get(controllers.itemController.findOne)
+    // .get(itemController.findOne)
     findOne: (req, res) => {
         db.Item
-            .find({ upc: upc })
+            // .find({ upc: upc })
+            .find({ upc: req.params.upc })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
 
     // creates an item
-    // .post(controllers.itemController.create)
+    // .post(itemController.create)
     create: (req, res) => {
         db.Item
             .create(req.body)
@@ -42,10 +37,10 @@ module.exports = {
     },
 
     // updates an item
-    // .put(controllers.itemController.update)
+    // .put(itemController.update)
     update: (req, res) => {
         db.Item
-            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .findOneAndUpdate({ upc: req.params.upc }, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
@@ -57,6 +52,6 @@ module.exports = {
             .findById({ upc: req.params.upc})
             .then(dbModel => dbModel.remove())
             .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err))
+            .catch(err => res.status(422).json(err));
     }
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,21 @@
-var router = require("express").Router();
-var path = require("path");
-var itemRoutes = require("./items");
-var userRoutes = require("./users");
-// var storeRoutes = require("./stores");
-var transactionRoutes = require("./transactions");
+const router = require("express").Router();
 
+const itemRoutes = require("./items");
+const userRoutes = require("./users");
+// const storeRoutes = require("./stores");
+const transactionRoutes = require("./transactions");
+
+
+// Item routes
 router.use("/items", itemRoutes);
+
+// User routes
 router.use("/users", userRoutes);
+
+// Store routes
 // router.use("/stores", storeRoutes);
+
+// Transaction routes
 router.use("/transactions", transactionRoutes);
 
 module.exports = router;
