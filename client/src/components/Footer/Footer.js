d0fcd01f63781f5c972aa7e3f57e7a86dd7e1081
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-08-11 14:03:24 -0400

    added footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+
+const Footer = () => (
+   <footer className="footer text-faded text-center py-5">
+        <p className="m-0 small">Copyright&nbsp;©
+            <a href="team.html"> TeamPopUp</a>
+        </p>
+    </footer>
+ );
+
+export default Footer;
